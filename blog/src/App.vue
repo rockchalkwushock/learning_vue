3af27c82797d43cbc9db00881819d4da32e37625
Cody Brunner <rockchalkwushock@icloud.com>
2018-03-19 10:11:11 -0500

    blog exercise with computed & methods

---
+++ blog/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Blog/>
+  </div>
+</template>
+
+<script>
+import Blog from './components/Blog'
+
+export default {
+  name: 'App',
+  components: {
+    Blog
+  }
+}
+</script>
+
+<style>
+#app {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  font-family: 'Work Sans', sans-serif;
+}
+</style>
